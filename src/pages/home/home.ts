1a672ea76591413411d589dc7e7a2de33d1f5539
Oscar Luis Muñoz Martínez <oscarmunozdev@gmail.com>
2018-04-23 18:26:23 +0200

    Just a Greeting Application

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  personName: string;
 
+  constructor(private toast: ToastController) {
+
+  }
+
+  showGreeting(name: string){
+    this.toast.create({
+      message: `Hello ${name}`,
+      duration: 3000
+    }).present();
   }
 
 }
