c430e75ecd6b0bec039f8b7cb9c4349f7ecd6d6c
Oscar Luis Muñoz Martínez <oscarmunozdev@gmail.com>
2018-04-23 10:15:56 +0200

    Adding Data through nav.params

---
+++ src/pages/second/second.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SecondPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-second',
+  templateUrl: 'second.html',
+})
+export class SecondPage {
+
+  message: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.message = this.navParams.get('message');
+    alert(this.message);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SecondPage');
+  }
+
+}
